17a569e798d851691d77e3064ebe39adc0b78f84
Matthew Elwell <matthew.elwell@flagsmith.com>
2024-03-25 15:06:45 +0000

    Update to ProviderFatalError

--- openfeature_flagsmith/exceptions.py
+++ openfeature_flagsmith/exceptions.py
@@ -1,23 +1,12 @@
-from openfeature.exception import ErrorCode, OpenFeatureError
+from openfeature.exception import OpenFeatureError, ProviderFatalError
 
 
 class FlagsmithProviderError(OpenFeatureError):
     pass
 
 
-class FlagsmithConfigurationError(OpenFeatureError):
+class FlagsmithConfigurationError(ProviderFatalError):
     """
     This exception should be raised when the Flagsmith provider has not been set up correctly
-
-    TODO: this should inherit from ProviderFatalError when available in openfeature.
     """
-
-    def __init__(self, error_message: str = None):
-        """
-        Constructor for the FlagsmithConfigurationError.  The error code for
-        this type of exception is ErrorCode.PROVIDER_NOT_READY.
-        @param error_message: a string message representing why the error has been
-        raised
-        @return: a FlagsmithConfigurationError exception
-        """
-        super().__init__(ErrorCode.PROVIDER_NOT_READY, error_message)
+    pass
