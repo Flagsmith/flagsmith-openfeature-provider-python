5268e49cab2a58f7ed136d2e8cc1e8b5d4728f41
Matthew Elwell <matthew.elwell@flagsmith.com>
2025-06-02 14:53:07 +0100

    Update provider method signatures to conform to specification

--- openfeature_flagsmith/provider.py
+++ openfeature_flagsmith/provider.py
@@ -51,45 +51,45 @@ class FlagsmithProvider(AbstractProvider):
 
     def resolve_string_details(
         self,
-        key: str,
+        flag_key: str,
         default_value: str,
-        evaluation_context: EvaluationContext = EvaluationContext(),
+        context: EvaluationContext = EvaluationContext(),
     ) -> FlagResolutionDetails[str]:
-        return self._resolve(key, FlagType.STRING, default_value, evaluation_context)
+        return self._resolve(flag_key, FlagType.STRING, default_value, context)
 
     def resolve_integer_details(
         self,
-        key: str,
+        flag_key: str,
         default_value: int,
-        evaluation_context: EvaluationContext = EvaluationContext(),
+        context: EvaluationContext = EvaluationContext(),
     ) -> FlagResolutionDetails[int]:
-        return self._resolve(key, FlagType.INTEGER, default_value, evaluation_context)
+        return self._resolve(flag_key, FlagType.INTEGER, default_value, context)
 
     def resolve_float_details(
         self,
-        key: str,
+        flag_key: str,
         default_value: float,
-        evaluation_context: EvaluationContext = EvaluationContext(),
+        context: EvaluationContext = EvaluationContext(),
     ) -> FlagResolutionDetails[float]:
-        return self._resolve(key, FlagType.FLOAT, default_value, evaluation_context)
+        return self._resolve(flag_key, FlagType.FLOAT, default_value, context)
 
     def resolve_object_details(
         self,
-        key: str,
+        flag_key: str,
         default_value: typing.Union[dict, list],
-        evaluation_context: EvaluationContext = EvaluationContext(),
+        context: EvaluationContext = EvaluationContext(),
     ) -> FlagResolutionDetails[typing.Union[dict, list]]:
-        return self._resolve(key, FlagType.OBJECT, default_value, evaluation_context)
+        return self._resolve(flag_key, FlagType.OBJECT, default_value, context)
 
     def _resolve(
         self,
-        key: str,
+        flag_key: str,
         flag_type: FlagType,
         default_value: typing.Any,
-        evaluation_context: EvaluationContext,
+        context: EvaluationContext,
     ) -> FlagResolutionDetails:
         try:
-            flag = self._get_flags(evaluation_context).get_flag(key)
+            flag = self._get_flags(context).get_flag(flag_key)
         except FlagsmithClientError as e:
             raise FlagsmithProviderError(
                 error_code=ErrorCode.GENERAL,
@@ -120,13 +120,13 @@ class FlagsmithProvider(AbstractProvider):
 
         raise TypeMismatchError(
             error_message="Value for flag '%s' is not of type '%s'"
-            % (key, flag_type.value)
+            % (flag_key, flag_type.value)
         )
 
-    def _get_flags(self, evaluation_context: EvaluationContext = EvaluationContext()):
-        if targeting_key := evaluation_context.targeting_key:
+    def _get_flags(self, context: EvaluationContext = EvaluationContext()):
+        if targeting_key := context.targeting_key:
             return self._client.get_identity_flags(
                 identifier=targeting_key,
-                traits=evaluation_context.attributes.get("traits", {}),
+                traits=context.attributes.get("traits", {}),
             )
         return self._client.get_environment_flags()
